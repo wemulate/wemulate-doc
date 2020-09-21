32066f2c5d9ad1c0f4f126cba488c354b95ceb70
Julian Klaiber <julian@klaiber.me>
2020-09-21 11:33:06 +0200

    Fix bug

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
+# add sourcecode to path
+import sys, os
+sys.path.insert(0, os.path.abspath('../src'))
